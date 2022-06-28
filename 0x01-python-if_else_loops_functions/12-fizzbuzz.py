24c2bf3f9842fb95ccc39c5997874e511818aae8
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-06-28 23:52:11 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # function that prints the numbers from 1 to 100 separated by a space
-# Created by bright
-.
+# Created by bright.
+
 """Print the numbers from 1 to 100 separated by a space.
   For multiples of three, print Fizz instead of the number
   For multiples of five, print Buzz instead of the number.
