0f8c769e19a9dd1a3dbc5972e3800e009c465e36
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-06-28 18:15:48 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 '#program that prints the ASCII alphabet, in lowercase, except q and e '
 
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) is != 'q' and chr(letter) is != 'e':
         print("{}".format(chr(letter)), end="")
