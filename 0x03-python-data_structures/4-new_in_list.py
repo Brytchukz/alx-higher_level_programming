7092e73062e96150ee845ddd652765c6a5e5e41a
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-07-03 13:57:51 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# function that replaces an element in a
+# list at a specific position without modifying 
+# the original list (like in C).
+# created by bright
+
+
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
