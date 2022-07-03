bfa539b8530bf4c92c7f97a2b0753437a716a95f
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-07-03 14:01:07 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# function that replaces an element in a
-# list at a specific position without modifying 
-# the original list (like in C).
+# function that replaces an element in a list
 # created by bright
 
 
