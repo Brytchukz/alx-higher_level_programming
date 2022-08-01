bfba51462496be76cc2ab754700a88b2f644486c
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-08-01 23:41:04 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,9 +8,3 @@ class BaseGeometry:
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Sec
