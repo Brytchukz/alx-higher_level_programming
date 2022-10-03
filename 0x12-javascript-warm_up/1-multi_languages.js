e7cbc5bee58c73dcc0fd6edfe35c29d0fd079956
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-10-03 21:42:19 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let A = "C is fun";
-B = "python is cool";
-C = "JavaScript is amazing";
+let A = 'C is fun';
+B = 'python is cool';
+C = 'JavaScript is amazing';
 console.log(A);
 console.log(B);
 console.log(C);
